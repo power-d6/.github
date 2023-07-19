ae41e6ca7aedf9a98f767b18ffca044a92f706bf
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-07-19 14:54:29 +0200

    cleanup code

--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,1 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as github from "@pulumi/github";
 
-import {repositories, createRepository} from "./repositories";
-
-export const createdRepositories = repositories.map(createRepository);
